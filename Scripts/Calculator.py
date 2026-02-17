adcbf40bb3c41e403ccede5a0d4330504610ca57
ArteXiz <115343725+ArteXiz@users.noreply.github.com>
2026-02-17 15:05:30 +0400

    Delete Scripts directory

--- Scripts/Calculator.py
+++
@@ -1,7 +0,0 @@
-import itertools
-
-with open("codes.txt", "w", encoding="utf-8") as file:
-    for code in itertools.product(range(6), repeat=5):
-        file.write("".join(map(str, code)) + "\n")
-
-print("Файл codes.txt создан. Все коды записаны построчно.")
